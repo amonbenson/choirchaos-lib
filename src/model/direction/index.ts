488395863954e422f0868231e46a2605c57fc840
Amon Benson <amon.k.benson@campus.tu-berlin.de>
2026-04-04 01:25:49 +0200

    updates to main song

--- src/model/direction/index.ts
+++ src/model/direction/index.ts
@@ -1,3 +1,7 @@
-export { Direction as MeasureEvent, type DirectionJson as MeasureEventJson } from "./base.js";
+export { Direction, type DirectionJson } from "./base.js";
+export { Cut, type CutJson } from "./cut.js";
 export { Marker, type MarkerJson } from "./marker.js";
 export { Repeat, type RepeatJson } from "./repeat.js";
+export { TempoChange, type TempoChangeJson } from "./tempoChange.js";
+export { TimeSignatureChange, type TimeSignatureChangeJson } from "./timeSignatureChange.js";
+export { Vamp, type VampJson } from "./vamp.js";
--- src/model/song.ts
+++ src/model/song.ts
@@ -1,4 +1,9 @@
 import type { Numbering } from "../music/numbering.js";
+import { DirectionList } from "./direction/base.js";
+import { Cut } from "./direction/cut.js";
+import { Marker } from "./direction/marker.js";
+import { Repeat } from "./direction/repeat.js";
+import { Vamp } from "./direction/vamp.js";
 import { MeasureList } from "./measure.js";
 
 const BrandTypeId: unique symbol = Symbol.for("effect/Brand");
@@ -6,16 +11,25 @@ const BrandTypeId: unique symbol = Symbol.for("effect/Brand");
 export type SongId = string & { readonly [BrandTypeId]: { readonly SongId: "SongId" } };
 export type SongNumber = Numbering & { readonly [BrandTypeId]: { readonly SongNumber: "SongNumber" } };
 
+export type SongDirections = {
+  markers: DirectionList<Marker>;
+  repeats: DirectionList<Repeat>;
+  vamps: DirectionList<Vamp>;
+  cuts: DirectionList<Cut>;
+  segue: boolean;
+};
+
 export default class Song {
   constructor(
     public readonly id: SongId,
     public number: SongNumber,
     public title: string,
     public measures: MeasureList = new MeasureList(),
-    public events: SongEvents = {
-      markers: new MeasureEventList<MarkerEvent>(),
-      repeats: new MeasureEventList<RepeatEvent>(),
-      jumps: new MeasureEventList<JumpEvent>(),
+    public directions: SongDirections = {
+      markers: new DirectionList<Marker>(),
+      repeats: new DirectionList<Repeat>(),
+      vamps: new DirectionList<Vamp>(),
+      cuts: new DirectionList<Cut>(),
       segue: false,
     },
   ) {}
